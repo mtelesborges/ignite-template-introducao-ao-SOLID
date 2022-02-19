4fc0f72775a3333dbef9b46e959a006cff804ea5
Mailson Teles Borges <20091@mtcorp.com.br>
2022-02-19 20:03:53 -0300

    initial

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,57 @@
+import { User } from "../../model/User";
+import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private users: User[];
+
+  private static INSTANCE: UsersRepository;
+
+  private constructor() {
+    this.users = [];
+  }
+
+  public static getInstance(): UsersRepository {
+    if (!UsersRepository.INSTANCE) {
+      UsersRepository.INSTANCE = new UsersRepository();
+    }
+
+    return UsersRepository.INSTANCE;
+  }
+
+  create({ name, email }: ICreateUserDTO): User {
+    
+    const user = Object.assign(new User(), {
+      name,
+      email
+    })
+    
+    this.users.push(user);
+
+    return user;
+  }
+
+  findById(id: string): User | undefined {
+    return this.users.find(user => user.id == id);
+  }
+
+  findByEmail(email: string): User | undefined {
+    return this.users.find(user => user.email == email);
+  }
+
+  turnAdmin(receivedUser: User): User {
+    this.users.map(user => {
+      if(user.id === receivedUser.id){
+        user.admin = true;
+      }
+      return user;
+    })
+
+    return this.findById(receivedUser.id);
+  }
+
+  list(): User[] {
+    return this.users;
+  }
+}
+
+export { UsersRepository };
